5639e2ad530e88a899ab61766a43e37399cc38b4
rama1990br <rama1990.br@gmail.com>
2017-11-01 20:19:08 -0700

    changes made according to hrishi's feedback

--- calendar.js
+++ calendar.js
@@ -1,12 +1,14 @@
+//function to display the table
 function myTable() {
-    var table = document.createElement('table');
-    document.body.appendChild(table);
+    var table = document.createElement('table'); 
     var thElements = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    //function to display the days of the week as the header
     for (var k = 0; k < 7; k++) {
         var th = document.createElement('th');
         th.innerText = thElements[k];
         table.appendChild(th);
     }
+    //function to display hourly blocks of each day for one week
     for (var i = 0; i < 24; i++) {
         var tr = document.createElement('tr');
         table.appendChild(tr);
@@ -17,5 +19,6 @@ function myTable() {
             tr.appendChild(td);
         }
     }
+    document.body.appendChild(table);
 }
 myTable();
