4db9ebf22846ce6cbabaafb5b15abc1763fe168d
rama <rama1990.br@gmail.com>
2018-01-09 14:13:35 -0800

    Changes related to review comments provided on 2/12
    * Storing date and time of event as epoch time in the database
    * Removed commented out code
    * Created a new file, 'db_functions.js',  with the database logic to separate it from the request handling logic
    * Surrounded bodies of if and else with {} in every instance
    * Declared all variables at the top of their respective scopes
    * Provided spaces wherever necessary
    * Replaced switch with foreach wherever possible
    * Passed database data directly to the updateTable method
    
    Still need to :
    * Remove the extra cells at the end of each row where rowspan has been used
    * Handle errors with respect to database

--- app.js
+++ app.js
@@ -2,36 +2,37 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mysql = require('mysql');
+var db = require('./db_functions.js');
+
 const PORT = 3000;
 
-
-http.createServer(function (request, response) {
-    //console.log(request.url);
-    
-    if (request.url === '/favicon.ico') {           //code to handle favicon.ico image
-        response.writeHead(200, {'Content-Type': 'image/x-icon'} );
-        response.end();
-        return;
-    }
-    var con = mysql.createConnection({
+var con = mysql.createConnection({
         host: "localhost",
         user: "custom",
         password: "password",
         database: "calendar_db"
     });
 
+http.createServer(function(request, response) {
+    if (request.url === '/favicon.ico') {           //code to handle favicon.ico image
+        response.writeHead(200, {'Content-Type': 'image/x-icon'});
+        response.end();
+        return;
+    }
     if (request.method === 'POST' && request.url === '/') {
         var body = '';
-
-        request.on('data', function (data) {
-            body =JSON.parse(data);
-            if (body.length > 1e6){
+        request.on('data', function(data) {
+            body = JSON.parse(data);
+            if(body.length > 1e6) {
                 request.connection.destroy();
             }
-            con.query('INSERT INTO dummyTable SET ?' ,body, function (err, result) {
-                if (err) throw err;
+            db.data.addEvent(con,body,dbCallbackAddEvent);
+            function dbCallbackAddEvent(err, result) {
+                if (err) {
+                    throw err;
+                } 
                 console.log("1 record inserted");
-            });
+            }
         });
     }
   
@@ -54,17 +55,19 @@ http.createServer(function (request, response) {
         extName = filePath.split('.')[1];
     }
     
-    if(request.url.includes('/getJSONResponse') && request.method === 'GET'){
+    if(request.url.includes('/getjsonresponse') && request.method === 'GET') {
         var result;
-        con.query('SELECT * FROM dummyTable', (err,rows) => {
-            if(err) throw err;
+        db.data.retrieveEvent(con, dbCallbackRetrieveEvent);
+        function dbCallbackRetrieveEvent(err, rows) {
+            if (err) {
+                throw err;
+            } 
             console.log('Data received from Db:\n');
             response.writeHead(200, { 'Content-Type': 'application/json' });
             response.end(JSON.stringify(rows), 'utf-8');
-        });
-  
+        }
     }
-    else{
+    else {
         fs.readFile('./' + filePath.split('?')[0], function (error, content) {
             if (error) {
                 throw error;
--- calendar.js
+++ calendar.js
@@ -10,22 +10,30 @@ $("#calendar-id").on('click', 'td', displayModal);
 
 //cancelModal is defined outside displayModal cause otherwise, it's entry is made in the event handler table each time it is 
 //encountered in the displayModal method and therefore is called multiple times on each cancel button click. 
-function cancelModal(){
+function cancelModal() {
     $("#favDialog")[0].close();
 }
 
-function confirmModal(event){
-
+function getSelectedDate(todaysDate, dayOfWeekNumber) {
+    var diffInNumberOfDays = todaysDate.getDay() - dayOfWeekNumber;
+    todaysDate.setHours(-24 * diffInNumberOfDays);
+    return todaysDate;
+}
+function confirmModal(event) {
+    var selectedDate = getSelectedDate(new Date(), event.data.param1);
+    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var epochStartTimeOfEvent = (new Date(monthNames[selectedDate.getMonth()] + " " + selectedDate.getDate() + ", " + selectedDate.getFullYear() + " " + $("#startTime option:selected").text() + ":00")).getTime()/1000.0;
+    var epochEndTimeOfEvent = (new Date(monthNames[selectedDate.getMonth()] + " " + selectedDate.getDate() + ", " + selectedDate.getFullYear() + " " + $("#endTime option:selected").text() + ":00")).getTime()/1000.0;
     var sendInfo = {
-           eventName: $("#eventName").val(),
-           startTime: $("#startTime option:selected").text() + event.data.param1,
-           endTime: $("#endTime option:selected").text() + event.data.param1, 
-           locationName: $("#locationName").val()
-       };
+        eventName: $("#eventName").val(),
+        startTime: epochStartTimeOfEvent,
+        endTime: epochEndTimeOfEvent,
+        locationName: $("#locationName").val()
+    };
 
-    // var userData = $("#eventName").val() + " " + $("#locationName").val();
     $.ajax({
-        url: './',
+        url: '/',
         method: 'POST',
         data: JSON.stringify(sendInfo),
         cache: false,
@@ -33,152 +41,84 @@ function confirmModal(event){
             console.log("done!");
         }
     });
-
     myTable();
 }
 
 function displayModal() {
- var startTime = this.parentNode.innerText;
-
-  $("#favDialog")[0].showModal(); //showModal function can be applied only to a HTMLDialogMethod and not to a jQuery object, therefore $("#favDialog")[0]
-  
- 
-  $("#startTime option").each(function() {   
-        if(startTime.includes(this.value)){
-            $("#"+this.id).attr("selected","selected");
+    var selectedStartTime = this.parentNode.innerText;
+    var param1;
+    $('#favDialog')[0].showModal(); //showModal function can be applied only to a HTMLDialogMethod and not to a jQuery object, therefore $("#favDialog")[0]
+    $("#startTime option").each(function() {   
+        if(selectedStartTime.includes(this.value)) {
+            $("#" + this.id).attr("selected","selected");
         }
-  });
-  // Form cancel button closes the dialog box
-  $("#cancel").click(cancelModal); 
-var param1;
- $("#confirm").click({param1: this.id}, confirmModal);
+    });
+    // Form cancel button closes the dialog box
+    $("#cancel").click(cancelModal); 
+    $("#confirm").click({param1: parseInt( $(this).index() - 1)}, confirmModal);
 }
 
 
 //function to create the skeleton of the calendar
 function createTable() {
-    var table = document.createElement('table');
+    var table, thElements, k, th, i, tr, td, j;
+    table = document.createElement('table');
     table.id = "calendar-id";
-    var thElements = ['Hour of day', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    for (var k = 0; k < 8; k++) {
-        var th = document.createElement('th');
-        th.innerText = thElements[k];
+    thElements = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    th = document.createElement('th');
+    th.innerText = 'Hour of day';
+    table.appendChild(th);
+    thElements.forEach(function(day, i) {
+        th = document.createElement('th');
+        th.id = i + day;
+        th.innerText = day;
         table.appendChild(th);
-    }
-    for (var i = 0; i < 24; i++) {
-        var tr = document.createElement('tr');
+    });
+
+    for (i = 0; i < 24; i++) {
+        tr = document.createElement('tr');
         tr.id = i;
         table.appendChild(tr);
-        var td = document.createElement('td');
-        td.id="hour"+i;
+        td = document.createElement('td');
         td.innerText = i + ":00";
         tr.appendChild(td);
-
-        for (var j = 1; j < 8; j++) {
-            var td = document.createElement('td');
-            switch(j)
-            {
-                case 1: td.id="sunday"+i;
-                        break;
-                case 2: td.id="monday"+i;
-                        break;
-
-                case 3: td.id="tuesday"+i;
-                        break;
-                case 4: td.id="wednesday"+i;
-                        break;
-                case 5: td.id="thursday"+i;
-                        break;
-                case 6: td.id="friday"+i;
-                        break;
-                case 7: td.id="saturday"+i;
-                        break;
-
-
-            }
-            updateCellWithNoAppointment(td);
+        thElements.forEach(function(day, j) {
+            td = document.createElement('td');
+            td.id = day + i;
             tr.appendChild(td);
-        }
+        });
     }
     return table;
 }
 //function which makes an ajax call to get the values from the json file which contains the hours for which appointments have been booked
 function getScheduleAndUpdateTable(table) {
-    var eventname=[];
-    var starttime = [];
-    var endtime=[];
-    var locationname=[];
     $.ajax({
-        url: '/getJSONResponse',
+        url: '/getjsonresponse',
         type: 'get',
         cache: false,
         success: function (data) {
-            $.each(data, function (i, item) {
-                eventname[i]=item.eventName;
-                starttime[i] = item.startTime;
-                endtime[i]= item.endTime;
-                locationname[i]=item.locationName;
-            });
-            updateTable(table,eventname, starttime,endtime,locationname);
-        }
+            updateTable(table,data);
+        }   
     });
 }
 //function to update the table according to the booked appointments
-function updateTable(table, eventname, starttime, endtime, locationname) {
-    for(var i=0;i<starttime.length;i++){
-        var startTimeHour=starttime[i].split(':')[0];
-        var startTimeDay=starttime[i].split(':')[1].substring(2);
-        var endTimeEvent=endtime[i].split(':')[0];
-        var endTimeDay=endtime[i].split(':')[1].substring(2);
-
-        var startRow=document.getElementById(starttime[i].split(':')[0]);
-        var endRow=document.getElementById(endtime[i].split(':')[0]);
-
-        for(var j=0;j<8;j++){
-            if(startRow.childNodes[j].id.includes(startTimeDay)){
-                //var startCell=document.getElementById(startTimeDay+starttime[i].substring(0,1));
-                //startCell.attr('rowspan',endtime[i].substring(0,1)-starttime[i].substring(0,1));
-                $("#"+startTimeDay).attr('rowspan',endtime[i].split(':')[0]-starttime[i].split(':')[0]+1);
-                $("#"+startTimeDay).text(eventname[i]+" at "+locationname[i]);
+function updateTable(table, data) {
+    var eventName, startRow, startTimeDay, locationName, startTimeHour, endTimeHour;
+    $.each(data, function (i, item) {
+        eventName = item.eventName;
+        locationName = item.locationName;
+        startTimeDay = (new Date(item.startTime * 1000)).toLocaleString('en-US', {weekday: 'short'});
+        startTimeHour = (new Date(item.startTime * 1000)).toLocaleString().split(' ')[2] == 'AM' ? (new Date(item.startTime * 1000)).toLocaleString().split(' ')[1].split(':')[0] : parseInt((new Date(item.startTime * 1000)).toLocaleString().split(' ')[1].split(':')[0]) + 12;
+        endTimeHour = (new Date(item.endTime * 1000)).toLocaleString().split(' ')[2] == 'AM' ? (new Date(item.endTime * 1000)).toLocaleString().split(' ')[1].split(':')[0] : parseInt((new Date(item.endTime * 1000)).toLocaleString().split(' ')[1].split(':')[0]) + 12;
+        startRow = document.getElementById(startTimeHour);
+        numberOfHours = endTimeHour - startTimeHour;
+        $.each(startRow.childNodes, function(j, item) {
+            if(item.id.includes(startTimeDay)) {
+                $("#" + item.id).attr('rowspan',endTimeHour - startTimeHour + 1);
+                $("#" + item.id).text(eventName + " at " + locationName);
             }
-        }
-
-    }
-    // for (var i = 0; i < 24; i++) {
-    //     var tr = document.getElementById(i);
-    //     if(starttime.includes(tr.childNodes[0].innerText)){
-
-    //     }
-    //     for (var j = 0; j < 7; j++) {
-    //         var td = tr.getElementsByTagName('td');
-
-    //         if (starttime[j] === null) {
-    //             updateCellWithNoAppointment(td[j + 1]);
-    //         }
-    //         // else {
-    //         //     var hourMatchFlag = 0;
-    //         //     for (var k = 0; k < time[j].length; k++) {
-    //         //         if (time[j][k] == i) {
-    //         //             updateCellWithAppointment(td[j + 1]);
-    //         //             hourMatchFlag = 1;
-    //         //             break;
-    //         //         }
-    //         //     }
-    //         //     if (hourMatchFlag == 0) {
-    //         //         updateCellWithNoAppointment(td[j + 1]);
-    //         //     }
-
-    //         // }
-    //     }
-
-    // }
+        });
+    });
 }
-//function to display an empty cell in case of no booked appointment
-function updateCellWithNoAppointment(td) {
-    td.innerText = "";
-}
-//function to display the booked appointment
-function updateCellWithAppointment(td) {
-    td.innerText = "meeting with hrishi";
-}
+
+
